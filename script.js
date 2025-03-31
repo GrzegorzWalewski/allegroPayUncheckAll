6c6064d1d189800ae05f85e5b2b05480adbae02b
Grzojda <grzojda@gmail.com>
2025-03-31 09:49:20 +0200

    make extension

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-var checkboxes = document.getElementById('tab_0').getElementsByTagName('input');
+const ul = document.querySelector("m-tabs").querySelector('ul');
 
-for (checkbox of checkboxes) {
-  checkbox.click();
+if (ul) {
+  const existingLi = ul.querySelector('li:last-child');
+
+  const newLi = document.createElement('li');
+  const newButton = document.createElement('button');
+
+  if (existingLi) newLi.className = existingLi.className;
+
+  newButton.removeAttribute('aria-selected');
+  newButton.removeAttribute('aria-controls');
+
+  const img = document.createElement('img');
+  img.src = chrome.runtime.getURL('icon.png');
+  img.alt = 'Logo';
+  img.style.width = '35px';
+  img.style.height = '35px';
+
+  newButton.style.border = 'unset';
+  newButton.style.padding = 'unset';
+  newButton.style.cursor = 'pointer';
+  
+  newButton.appendChild(img);
+  
+  newButton.addEventListener("click", () => {
+    const checkboxes = document.getElementById("tab_0").getElementsByTagName("input");
+    for (const checkbox of checkboxes) {
+      checkbox.click();
+    }
+  });
+  
+  newLi.appendChild(newButton);
+  ul.insertBefore(newLi, ul.firstChild);
 }
